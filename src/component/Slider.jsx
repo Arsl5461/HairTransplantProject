1be36a3d944643b578acb2e2eb8921b100b93df8
Arslan Akmal <Arslan.akmal@arhamsoft.org>
2023-08-15 21:30:28 +0500

    Gallery Added

--- src/component/Slider.jsx
+++ src/component/Slider.jsx
@@ -1,30 +1,27 @@
-import React, { useEffect } from 'react';
+import React, { useState } from "react";
+import ReactCompareImage from "react-compare-image";
 import "./Slider.css";
 
-function Sider() {
-  useEffect(() => {
-    const divisor = document.getElementById("divisor");
-    const slider = document.getElementById("slider");
-    
-    function moveDivisor() {
-      divisor.style.width = slider.value + "%";
-    }
-    
-    slider.addEventListener("input", moveDivisor);
-    
-    return () => {
-      slider.removeEventListener("input", moveDivisor);
-    };
-  }, []);
+function App() {
+  const before =
+    "https://upload.wikimedia.org/wikipedia/commons/f/f5/Poster-sized_portrait_of_Barack_Obama.jpg";
+  const after =
+    "https://hindalkindi1992.files.wordpress.com/2013/11/portrait_eyes_23.jpg";
+
+  // You can use the useState hook to manage the state
+  const [hasError, setHasError] = useState(false);
+
+  // You don't need getDerivedStateFromError and componentDidCatch hooks in functional components
 
   return (
-    <div id="comparison">
-      <figure>
-        <div id="divisor"></div>
-      </figure>
-      <input type="range" min="0" max="100" defaultValue="50" id="slider" />
+    <div className="mainWrapper">
+      <div className="contentWrapper">
+        <ReactCompareImage leftImage={before} rightImage={after} />
+        <ReactCompareImage leftImage={before} rightImage={after} />
+        <ReactCompareImage leftImage={before} rightImage={after} />
+      </div>
     </div>
   );
 }
 
-export default Sider;
+export default App;
